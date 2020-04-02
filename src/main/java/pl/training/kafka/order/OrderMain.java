e84704b0fdc5b0273c49e2a3e80b66235d9411a9
Mariusz Strzelecki <mariusz@acast.com>
2020-04-02 14:19:35 +0200

    spring factories in shipping service

--- src/main/java/pl/training/kafka/order/OrderMain.java
+++ src/main/java/pl/training/kafka/order/OrderMain.java
@@ -1,7 +1,19 @@
 package pl.training.kafka.order;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 
@@ -9,5 +21,21 @@ public class OrderMain {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class);
+    }
+
+    private ConsumerFactory consumerFactory() {
+        Map<String, Object> params = new HashMap<>();
+        params.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // kafka?
+        params.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        params.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        return new DefaultKafkaConsumerFactory(params);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> containerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 }
