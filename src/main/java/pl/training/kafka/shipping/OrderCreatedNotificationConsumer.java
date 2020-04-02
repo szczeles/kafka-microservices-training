f563a399786877e5f67b587ddf623d477f654839
Mariusz Strzelecki <mariusz@acast.com>
2020-04-02 11:06:39 +0200

    Simple stdout consumerg

--- src/main/java/pl/training/kafka/shipping/OrderCreatedNotificationConsumer.java
+++ src/main/java/pl/training/kafka/shipping/OrderCreatedNotificationConsumer.java
@@ -1,13 +1,46 @@
 package pl.training.kafka.shipping;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Properties;
+
 @Service
 public class OrderCreatedNotificationConsumer implements ApplicationRunner {
+    private final KafkaConsumer<String, String> consumer;
+
+    public OrderCreatedNotificationConsumer() {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092"); // or localhost:9092
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "shipping-service");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Arrays.asList("order-created"));
+//        Collections.singletonList("order-created");
+    }
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.printf("test");
+
+        System.out.println("Consumer started");
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record);
+            }
+        }
     }
 }
