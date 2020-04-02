6eeba80a224deddffa8593499d595c324a4053d2
Mariusz Strzelecki <mariusz@acast.com>
2020-04-02 16:24:25 +0200

    user service

--- src/main/java/pl/training/kafka/pl/training/kafka/user/UserMain.java
+++ src/main/java/pl/training/kafka/pl/training/kafka/user/UserMain.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserMain {
     public static void main(String[] args) {
+        System.setProperty("server.port", "9000");
         SpringApplication.run(UserMain.class);
     }
 }
