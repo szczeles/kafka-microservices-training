8c8e9bf924ae296083c50d1886c8676c2411eca6
Mariusz Strzelecki <mariusz@acast.com>
2020-04-02 09:00:22 +0200

    synchronous sender

--- src/main/java/pl/training/kafka/order/OrderCreatedNotificationSender.java
+++ src/main/java/pl/training/kafka/order/OrderCreatedNotificationSender.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class OrderCreatedNotificationSender {
@@ -25,6 +26,12 @@ public class OrderCreatedNotificationSender {
 
     public void send(String orderId) {
         ProducerRecord producerRecord = new ProducerRecord("order-created", orderId, String.format("New order created: %s", orderId));
-        producer.send(producerRecord);
+        try {
+            producer.send(producerRecord).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
